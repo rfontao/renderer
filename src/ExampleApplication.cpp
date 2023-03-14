d6c3dbc7c0b096dd13af03e3d04514cfed7c2d38
rfontao <ricardo.fontao@gmail.com>
2023-03-14 23:17:11 +0000

    Enabled validation layers

--- src/ExampleApplication.cpp
+++ src/ExampleApplication.cpp
@@ -1,19 +1,25 @@
 #define GLFW_INCLUDE_VULKAN
+
 #include <GLFW/glfw3.h>
+
+#include <stdexcept>
+#include <vector>
+#include <iostream>
+#include <cstring>
 
 #include "ExampleApplication.h"
 
 
 void ExampleApplication::Run() {
+    PrintAvailableVulkanExtensions();
     InitWindow();
     InitVulkan();
     MainLoop();
     Cleanup();
-
 }
 
 void ExampleApplication::InitVulkan() {
-
+    CreateInstance();
 }
 
 void ExampleApplication::MainLoop() {
@@ -23,6 +29,8 @@ void ExampleApplication::MainLoop() {
 }
 
 void ExampleApplication::Cleanup() {
+    vkDestroyInstance(instance, nullptr);
+
     glfwDestroyWindow(window);
     glfwTerminate();
 }
@@ -33,6 +41,84 @@ void ExampleApplication::InitWindow() {
     glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
     glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
-    window = glfwCreateWindow(WIDTH, HEIGHT, "Experimental Renderer", nullptr, nullptr);
+    window = glfwCreateWindow((int) WIDTH, (int) HEIGHT, "Experimental Renderer", nullptr, nullptr);
+}
 
+void ExampleApplication::CreateInstance() {
+    if (enableValidationLayers && !CheckValidationLayerSupport()) {
+        throw std::runtime_error("Validation layers requested, but not available!");
+    }
+
+    VkApplicationInfo appInfo{};
+    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    appInfo.pApplicationName = "Experimental Renderer";
+    appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.pEngineName = "None";
+    appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.apiVersion = VK_API_VERSION_1_3;
+
+    VkInstanceCreateInfo createInfo{};
+    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    createInfo.pApplicationInfo = &appInfo;
+
+    // Retrieve extensions needed by GLFW
+    uint32_t glfwExtensionCount = 0;
+    const char **glfwExtensions;
+
+    glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+    createInfo.enabledExtensionCount = glfwExtensionCount;
+    createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+    if (enableValidationLayers) {
+        createInfo.enabledLayerCount = (uint32_t) validationLayers.size();
+        createInfo.ppEnabledLayerNames = validationLayers.data();
+    } else {
+        createInfo.enabledLayerCount = 0;
+    }
+
+    if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+        throw std::runtime_error("Failed to create Vulkan instance");
+    }
+}
+
+void ExampleApplication::PrintAvailableVulkanExtensions() {
+    uint32_t extensionCount = 0;
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+    std::vector<VkExtensionProperties> extensions(extensionCount);
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+
+    std::cout << "Available extensions:" << std::endl;
+    for (const auto &extension: extensions) {
+        std::cout << '\t' << extension.extensionName << std::endl;
+    }
+}
+
+bool ExampleApplication::CheckValidationLayerSupport() {
+    uint32_t layerCount;
+    vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+
+    std::vector<VkLayerProperties> availableLayers(layerCount);
+    vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+
+    std::cout << "Available validation extensions:" << std::endl;
+    for (const auto &layer: availableLayers) {
+        std::cout << '\t' << layer.layerName << std::endl;
+    }
+
+    for (const char *layerName: validationLayers) {
+        bool layerFound = false;
+        for (const auto &layerProperties: availableLayers) {
+            if (strcmp(layerName, layerProperties.layerName) == 0) {
+                layerFound = true;
+                break;
+            }
+        }
+
+        if (!layerFound)
+            return false;
+    }
+
+    return true;
 }
--- src/ExampleApplication.h
+++ src/ExampleApplication.h
@@ -3,6 +3,8 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
+#include <vector>
+
 class ExampleApplication {
 public:
     void Run();
@@ -12,9 +14,25 @@ private:
     void InitVulkan();
     void MainLoop();
     void Cleanup();
+    void CreateInstance();
+    void PrintAvailableVulkanExtensions();
+    bool CheckValidationLayerSupport();
+
+    VkInstance instance;
 
     GLFWwindow* window;
 
     const uint32_t WIDTH = 800;
     const uint32_t HEIGHT = 600;
+
+    const std::vector<const char*> validationLayers = {
+            "VK_LAYER_KHRONOS_validation"
+    };
+
+#ifdef NDEBUG
+    const bool enableValidationLayers = false;
+#else
+    const bool enableValidationLayers = true;
+#endif
+
 };
